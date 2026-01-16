Fixing flux-averaged xs in main code after fixing in notebook. Also clipping sampled ee so its always in range

pyNEWTON.py
@@ -96,8 +96,12 @@ if __name__ == "__main__":  # required on macOS / Windows
 
     #Calculate folded cross section
     folded_spectrum = np.multiply(xs_cm2_interp,dar_spectrum)
+
     #Calculate flux-averaged spectrum
-    flux_averaged_xs = np.average(folded_spectrum)
+    num = np.trapezoid(xs_cm2_interp * dar_spectrum, energies_MeV_interp)
+    den = np.trapezoid(dar_spectrum, energies_MeV_interp)
+    flux_averaged_xs = num / den
+
     flux_averaged_xs_naturalunits = flux_averaged_xs/utils.hbar_c_squared
     # Normalize
     neutrino_energy_probs = folded_spectrum / folded_spectrum.sum()

utils.py
@@ -463,7 +463,6 @@ def sampleEvent(args):
   #########
   #Generate input particles line
   #Incident neutrino
-  Enu = energies_MeV_interp[energy_idx]
   PXnu,PYnu,PZnu = calcMomentum(Enu,0,neutrino_direction)
   p_nu_vec = np.array([PXnu, PYnu, PZnu])
   inputParticles = []
@@ -489,6 +488,7 @@ def sampleEvent(args):
   #Estimate lepton energy (get approximate value)
   Ee_sampled = energies_MeV_interp[energy_idx] - event_excitation_energy_MeV - threshold
   Ee_idx = np.searchsorted(energies_MeV_interp,Ee_sampled)
+  Ee_idx = np.clip(Ee_idx, 0, len(energies_MeV_interp)-1)
   header["Ee_sampled"] = Ee_sampled
 
   #Use approximate lepton energy to sample lepton angle